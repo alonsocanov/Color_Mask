e4a03750eadfe6087ab4413bcdfa799ac271d083
alonsocanov <alonsocanov@hotmail.com>
2021-02-10 20:32:57 -0600

    Cleaned code

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
 import cv2
-from utils import hsvToCvHsv, imgResize
+from utils import hsvToCvHsv, imgResize, fileExist, colorGradient
 import numpy as np
 import argparse
 
 
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, default='',  help="Write the kind of tracking 'color' or 'face' or manual")
-    args = parser.parse_args()
-
-    if args.path:
-        # video path verification
-        pass
-    else:
-        webcam = cv2.VideoCapture(0)
-        if not webcam.isOpened():
-            webcam.release()
-            sys.exit("Error opening webcam")
-        ret, frame = webcam.read()
-        if not ret:
-            webcam = cv2.VideoCapture(1)
-            ret, frame = webcam.read()
-
-    width, height = imgResize(frame)
 
     hsv_lower = hsvToCvHsv(np.array([40, 40, 50]))
     hsv_upper = hsvToCvHsv(np.array([65, 100, 100]))
 
-    h_l, s_l, v_l = hsv_lower
-    h_u, s_u, v_u = hsv_upper
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, default='0',  help="Write the kind of tracking 'color' or 'face' or manual")
+    args = parser.parse_args()
 
-    img_range = cv2.resize(frame, (width, height))
-    h_gard = np.linspace(h_l, h_u, width)
-    s_gard = np.linspace(s_l, s_u, width)
-    v_gard = np.linspace(v_l, v_u, width)
 
-    img_range[:, :, 0] = np.tile(h_gard, (height, 1))
-    img_range[:, :, 1] = np.tile(s_gard, (height, 1))
-    img_range[:, :, 2] = np.tile(v_gard, (height, 1))
+    video = fileExist(args.path)
 
-    img_range = cv2.cvtColor(img_range.astype(np.uint8), cv2.COLOR_HSV2BGR)
+    ret, frame = video.read()
+    width, height = imgResize(frame)
+    img = cv2.resize(frame, (width, height))
+
+    img_range = colorGradient(img, hsv_lower, hsv_upper)
 
 
     win_name = 'Frame'
@@ -53,7 +35,7 @@ def main():
 
 
     while True:
-        ret, frame = webcam.read()
+        ret, frame = video.read()
         img = cv2.resize(frame, (width, height))
         blur = cv2.GaussianBlur(img, (5, 5), 0)
         hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
@@ -69,7 +51,7 @@ def main():
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
-    webcam.release()
+    video.release()
     cv2.destroyAllWindows()
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import os
+import sys
 
 # color normalization of HSV to OpenCV HSV
 def hsvToCvHsv(hsv: np.array) -> np.array:
@@ -17,3 +19,43 @@ def imgResize(img:np.ndarray, factor:float = None):
     if not factor:
         factor = 500 / w
     return int(factor * w), int(factor * h)
+
+
+def fileExist(path:str):
+    if path.isnumeric():
+        path = int(path)
+    video = cv2.VideoCapture(path)
+    ret, frame = video.read()
+    message = ''
+    if video.isOpened() == False or not ret:
+        if isinstance(path, int):
+            video = cv2.VideoCapture(path + 1)
+            ret, frame = video.read()
+            if video.isOpened() == False or not ret:
+                message = 'Error opening video stream'
+        else:
+            message = 'Error opening file'
+    if message:
+        video.release()
+        sys.exit(message)
+    return video
+
+def colorGradient(img, hsv_lower, hsv_upper):
+    height, width = img.shape[:2]
+    img = cv2.resize(img, (width, height))
+
+    h_l, s_l, v_l = hsv_lower
+    h_u, s_u, v_u = hsv_upper
+
+
+    h_gard = np.linspace(h_l, h_u, width)
+    s_gard = np.linspace(s_l, s_u, width)
+    v_gard = np.linspace(v_l, v_u, width)
+
+    img[:, :, 0] = np.tile(h_gard, (height, 1))
+    img[:, :, 1] = np.tile(s_gard, (height, 1))
+    img[:, :, 2] = np.tile(v_gard, (height, 1))
+
+    img = img.astype(np.uint8)
+
+    return cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
