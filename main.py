d8e510ba484f930ec233d124c3448c312f497a64
alonsocanov <alonsocanov@hotmail.com>
2021-02-10 19:29:47 -0600

    Added Color gradient

--- main.py
+++ main.py
@@ -13,19 +13,34 @@ def main():
         # video path verification
         pass
     else:
-    webcam = cv2.VideoCapture(0)
-    if not webcam.isOpened():
-        webcam.release()
-        sys.exit("Error opening webcam")
-    ret, frame = webcam.read()
-    if not ret:
-        webcam = cv2.VideoCapture(1)
+        webcam = cv2.VideoCapture(0)
+        if not webcam.isOpened():
+            webcam.release()
+            sys.exit("Error opening webcam")
         ret, frame = webcam.read()
+        if not ret:
+            webcam = cv2.VideoCapture(1)
+            ret, frame = webcam.read()
 
     width, height = imgResize(frame)
 
-    hsv_lower = hsvToCvHsv(np.array([45, 40, 20]))
-    hsv_upper = hsvToCvHsv(np.array([65, 90, 90]))
+    hsv_lower = hsvToCvHsv(np.array([40, 40, 50]))
+    hsv_upper = hsvToCvHsv(np.array([65, 100, 100]))
+
+    h_l, s_l, v_l = hsv_lower
+    h_u, s_u, v_u = hsv_upper
+
+    img_range = cv2.resize(frame, (width, height))
+    h_gard = np.linspace(h_l, h_u, width)
+    s_gard = np.linspace(s_l, s_u, width)
+    v_gard = np.linspace(v_l, v_u, width)
+
+    img_range[:, :, 0] = np.tile(h_gard, (height, 1))
+    img_range[:, :, 1] = np.tile(s_gard, (height, 1))
+    img_range[:, :, 2] = np.tile(v_gard, (height, 1))
+
+    img_range = cv2.cvtColor(img_range.astype(np.uint8), cv2.COLOR_HSV2BGR)
+
 
     win_name = 'Frame'
     cv2.namedWindow(win_name)
@@ -47,15 +62,14 @@ def main():
 
         mask_color = img.copy()
         mask_color[mask == 0] = 0
-
-        display_img = cv2.hconcat([img, mask_color])
+        display_img = cv2.hconcat([img_range, img, mask_color])
 
         cv2.imshow(win_name, display_img)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
-
+    webcam.release()
     cv2.destroyAllWindows()
 
 
